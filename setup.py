b4d4f6bd9470a31501cadede21be445159c8f081
Arthur <wangzhiqing@ata.net.cn>
2013-07-30 21:25:32 +0800

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='django-kombu',
+        version='0.1',
+        description='django-kombu',
+        author='Arthur',
+        author_email='largetalk@gmail.com',
+        url='',
+        packages=['django_kombu', ],
+        )
