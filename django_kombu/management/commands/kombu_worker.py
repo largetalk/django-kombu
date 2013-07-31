f521325503790badabbbf93ea297892e8cd4c3d0
Arthur <wangzhiqing@ata.net.cn>
2013-07-31 16:09:15 +0800

    worker

--- django_kombu/management/commands/kombu_worker.py
+++ django_kombu/management/commands/kombu_worker.py
@@ -1,5 +1,7 @@
 from optparse import make_option
 from django.core.management.base import BaseCommand, CommandError
+from django_kombu.worker import global_worker
+import daemon
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
@@ -9,4 +11,10 @@ class Command(BaseCommand):
     help = "kombu worker"
 
     def handle(self, *args, **options):
-        print "This is kombu worker"
+        print "kombu worker is running"
+        daemon = option_list.get('daemon')
+        if daemon:
+            with daemon.DaemonContext():
+                global_worker.run()
+        else:
+            global_worker.run()
