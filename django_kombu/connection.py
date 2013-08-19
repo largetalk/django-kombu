b6b98e758ae2b1bf95e888049f6570758722e3da
routing <saucer2007@gmail.com>
2013-08-19 15:04:44 +0800

    Add support for `queue_arguments` argument

--- django_kombu/connection.py
+++ django_kombu/connection.py
@@ -3,10 +3,13 @@ from kombu import Connection
 from kombu import Exchange, Queue
 import itertools
 
+def get_queue_arguments(setting):
+    return len(setting) >3 and setting[3] or {}
+
 default_connection = Connection(kombu_settings.TRANSPORT)
 default_exchange = Exchange(name = kombu_settings.EXCHANGE['name'],
                             type = kombu_settings.EXCHANGE['type'],
                             durable = kombu_settings.EXCHANGE['durable'],
                             auto_delete = kombu_settings.EXCHANGE['auto_delete'])
 
-task_queues = [ Queue(q[0], default_exchange, q[1]) for q in kombu_settings.QUEUES ]
+task_queues = [ Queue(q[0], default_exchange, q[1], queue_arguments=get_queue_arguments(q)) for q in kombu_settings.QUEUES ]
