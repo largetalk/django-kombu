0db266d1561c7b864c3c8935e029687e06edf704
Arthur <wangzhiqing@ata.net.cn>
2013-10-29 16:11:32 +0800

    refine setup.py and make python setup.py egg_info worked

---
+++ django_kombu/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.0.1'
+
+VERSION = __version__  # synonym
+
--- setup.py
+++ setup.py
@@ -1,8 +1,33 @@
-from distutils.core import setup
+from setuptools import setup
 import re
 import os
 import sys
 
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.match("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+    
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+            
+           
+version = get_version('django_kombu')
+                
 
 def get_packages(package):
     """
@@ -13,11 +38,25 @@ def get_packages(package):
             if os.path.exists(os.path.join(dirpath, '__init__.py'))]
 
 setup(name='django-kombu',
-        version='0.1',
+        version=version,
+        url='https://github.com/largetalk/django-kombu',
+        license = 'BSD',
         description='django-kombu',
         author='Arthur',
         author_email='largetalk@gmail.com',
-        url='',
         packages=get_packages('django_kombu'),
+        package_data = get_package_data('django_kombu'),
         install_requires=[],
+        classifiers = [
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Web Environment',
+            'Framework :: Django',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'Topic :: Internet :: WWW/HTTP',
+            ]
         )
+
